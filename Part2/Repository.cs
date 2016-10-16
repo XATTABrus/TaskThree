34b572da769255f49488885c16a03f5bca448b3d
XATTAB <XATTAB.rus@gmail.com>
2016-10-16 22:08:20 +0500

    Добавление решения второй части задания.

---
+++ Part2/Repository.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace TaskThree.Part2
+{
+    static class Repository
+    {
+        private static Hashtable _repository;
+        static Repository() { _repository = new Hashtable(); }
+
+        public static T Create<T>() where T : class, new() // Ввел ограничение на наличие ссылочного типа, посколькоку в 3 методе должна быть возможность возвращать null. 
+        {
+            var type = new T();
+            _repository.Add(Guid.NewGuid(), type);
+            return type;
+        }
+
+        public static Dictionary<Guid, T> GetAllGuidAndObjectForType<T>() where T: class
+        {
+            var resultDictionary = new Dictionary<Guid, T>();
+            foreach (var item in _repository.Keys)
+            {
+                var obj = _repository[item];
+                if (obj is T)
+                    resultDictionary.Add((Guid)item, (T)obj);
+            }
+            return resultDictionary;
+        }
+
+        public static T GetObjectByGuid<T> (Guid guid) where T : class
+        {
+            if (!_repository.Contains(guid))
+                return null;
+            return _repository[guid] as T;
+        }
+    }
+}
