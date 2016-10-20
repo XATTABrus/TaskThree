e7c473d6c1cd7c3eb1df1d1096370d9797ac9db1
Юрий <xattab.rus@gmail.com>
2016-10-20 21:48:03 +0500

    Доработка класса, с целью увеличить скорость работы.

--- Part2/Repository.cs
+++ Part2/Repository.cs
@@ -1,37 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace TaskThree.Part2
 {
     public static class Repository
     {
-        private static readonly Dictionary<Guid, object> _repository;
-        static Repository() { _repository = new Dictionary<Guid, object>(); }
+        private static readonly Dictionary<Type, Dictionary<Guid, object>> _repository;
+        static Repository() { _repository = new Dictionary<Type, Dictionary<Guid, object>>(); }
 
         public static T Create<T>() where T : class, new() // Ввел ограничение на наличие ссылочного типа, посколькоку в 3 методе должна быть возможность возвращать null. 
         {
-            var type = new T();
-            _repository.Add(Guid.NewGuid(), type);
-            return type;
+            var obj = new T();
+            var type = typeof (T);
+            var guid = Guid.NewGuid();
+
+            if (!_repository.ContainsKey(type))
+                _repository[type] = new Dictionary<Guid, object>();
+
+            _repository[type][guid] = obj;
+            return obj;
         }
 
         public static Dictionary<Guid, T> GetAllGuidAndObjectForType<T>() where T: class
         {
-            var resultDictionary = new Dictionary<Guid, T>();
-            foreach (var item in _repository.Keys)
-            {
-                var obj = _repository[item];
-                if (obj.GetType() == typeof(T))
-                    resultDictionary.Add((Guid)item, (T)obj);
-            }
-            return resultDictionary;
+            return _repository.ContainsKey(typeof(T)) ? _repository[typeof(T)].ToDictionary(x => x.Key, x => (T)x.Value) : new Dictionary<Guid, T>();
         }
 
         public static T GetObjectByGuid<T> (Guid guid) where T : class
         {
+            var type = typeof (T);
             object result;
-            _repository.TryGetValue(guid, out result);
 
+            _repository[type].TryGetValue(guid, out result);
             return result as T;
         }
     }
